d3e4b53e73a337e76ed9a7bcbc7521bca7917742
Brian <brian.n.norman@gmail.com>
2022-02-15 09:17:00 +0000

    Update GPShandler.py
    added VERBOSE option to reduce log messages. Changed non-blocking loop to use small sleeps because non-blocking method was sending CPU usage up to 100% all the time. Now the CPU usage peaks at <100% but now falls back to 1%.

--- dragino/GPShandler.py
+++ dragino/GPShandler.py
@@ -8,17 +8,14 @@ provides non-blocking access using GPSD
 """
 import logging
 from threading import Thread
-try:
-    from gps3 import agps3
-except:
-    import gpsd
+from gps3 import agps3
 
 import json
-from time import time
+from time import time,sleep
 from datetime import datetime,timedelta
 
 DEFAULT_LOG_LEVEL=logging.INFO
-
+VERBOSE=False #
 
 class GPS:
     def __init__(self, logging_level=DEFAULT_LOG_LEVEL,threaded=True,threadDelay=0.5):
@@ -35,27 +32,29 @@ class GPS:
         self.lon=None
         self.timestamp=None
         self.lastGpsReading=None
-        # used to control the thread execution
-        self.running=False
-        self.stopped=True
 
-    # gpsd settings - make sure gpsd is installed and running
-        self.gpsd_socket = agps3.GPSDSocket()
-        self.gpsd_socket.connect('127.0.0.1', 2947)
-        self.gpsd_socket.watch(gpsd_protocol='json', enable=True)
-        self.data_stream = agps3.DataStream()
+        # gpsd settings - make sure gpsd is installed and running
+        try:
+            self.gpsd_socket = agps3.GPSDSocket()
+            self.data_stream = agps3.DataStream()
+            self.gpsd_socket.connect() #'127.0.0.1', 2947) default
+            self.gpsd_socket.watch(gpsd_protocol='json', enable=True)
+            
+        except Exception as e:
+            self.logger.exception(f"Unable to setup GPSD {e}")
+
 
         if self.isThreaded:
+            self.running=False
+            self.stopped=True
+            self.logger.info("starting GPS updater thread")
             self.gpsThread=Thread(target=self._updater)
-            self.running=True
-            self.stopped=False
+  
             self.gpsThread.start()
-
-        self.sentences={}   # cache for GPSD sentences
 
     def __del__(self):
         if self.isThreaded:
-            self.logger.info("Stopping the GPS background thread")
+            self.logger.info("Stopping the background thread")
             self.running=False
             while not self.stopped:
                 pass
@@ -63,7 +62,7 @@ class GPS:
 
     def stop(self):
         self.__del__()
-
+        
     def get_gps(self):
         """
             return the cached GPS values
@@ -72,7 +71,7 @@ class GPS:
             these values updated
 
         """
-        self.logger.info("get_gps(): lat %s, lon %s, timestamp: %s", self.lat, self.lon, self.timestamp)
+        self.logger.debug(f"get_gps(): lat {self.lat}, lon {self.lon}, timestamp: {self.timestamp}")
         return self.lat, self.lon, self.timestamp, self.lastGpsReading
 
     def get_corrected_timestamp(self):
@@ -95,20 +94,27 @@ class GPS:
 
         :Param Delay: float (seconds)
         """
+        #sleep(Delay)
         start=time()
-        while time()<(start+Delay):
-            pass
+        while True:
+            if (time()-start)>=Delay:
+                break
+            sleep(0.1)
 
-
+ 
     def _updater(self):
         """
         GPS updater thread, samples GPS typically every 0.5 seconds
         """
+        self.logger.debug("GPS updater starting ")
+        self.running=True
+        self.stopped=False
         while self.running:
             self.update_gps()
             self.delay(self.threadLoopDelay)
+        self.logger.warn("GPS handler stopped")
         self.stopped=True
-
+		
     def update_gps(self):
         """
             Get the GPS position from the dragino,
@@ -123,35 +129,50 @@ class GPS:
 
         """
         try:
-            self.logger.info("update GPS")
+            if VERBOSE: 
+                self.logger.debug("update GPS")
             new_data = self.gpsd_socket.next()
+            
             if new_data is not None:
                 data = json.loads(new_data)
-                thisClass=data["class"]
-                self.logger.info(f"GPS new_data class {thisClass}")
-                if thisClass == "TPV" and data["mode"] >1 :
-                    self.lat = data["lat"]
-                    self.lon = data["lon"]
-                    self.timestamp = data["time"]
-                    self.lastGpsReading = time()
-
-                # cache the sentence
-                data["lastGpsReading"] = time() # record when reading took place
-                self.sentences[thisClass]=data
-
+                if VERBOSE:
+                    self.logger.info("GPS new_data class %s", data["class"])
+    
+                if data["class"] == "TPV":
+                    #print("TPV data",data)
+                    if data["mode"]==0 or data["mode"]==1:
+                        # no useable data or no fix
+                        self.logger.info("No GPS fix (yet)")
+                        return 
+                        
+                    try:
+                        self.lat = data["lat"]
+                        self.lon = data["lon"]
+                        self.timestamp = data["time"]
+                        print(f"Got lat {self.lat} lon {self.lon}") 
+                        self.lastGpsReading = time()
+                    except KeyError as e:
+                        self.logger.exception(f"unable to extract TPV data missing key error: {e}")
+                else:
+                    if VERBOSE:
+                        self.logger.debug(f"GPS message was {data}")
+                    pass
             else:
-                self.logger.info("No new gps data")
+                if VERBOSE:
+                    self.logger.debug("No new gps data")
+                
         except Exception as e:
-            self.logger.warning("update_gps() ignoring exception %s", e)
+            self.logger.warning(f"ignoring exception {e}")
 
-    def getSentence(self,which):
-        """
-        Returns the cached GPSD sentence or None
 
-        This allows callers to access more than just the device lat/lon and time
-        """
-        try:
-            return self.sentences[which]
-        except Exception as e:
-            self.logger.info(f"attempt to retrieve sentence {which} which doesn't exist")
-            return None
+if __name__=="__main__":
+    
+    gps=GPS()
+
+    start=time()
+    
+    
+    while True:
+        if time()-start>20:
+            break
+    
